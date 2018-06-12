12957c4176a1ad59b347b4444f947d8c26e4637f
King Pra <kingpra@Kings-MBP.carolina.rr.com>
2018-06-12 15:02:32 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -22,33 +22,55 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humdity,
-      description: data.weather[0].description,
-      error: ""
-
-
-    })
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter proper values"
+    });
   }
+}
   render () {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humdity={this.state.humdity}
-          description={this.state.description}
-          error={this.state.error}
-          />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather}/>
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                    />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,47 @@
 import React from 'react';
 
-class Weather extends React.Component {
-  render () {
-    return (
-      <div>
-      {this.props.city}
-      {this.props.country}
-      {this.props.temperature}
-      {this.props.humidity}
-      {this.props.description}
-      {this.props.error}
-      </div>
-    );
-  }
-};
+
+// class Weather extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//       {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+//       {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+//       {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+//       {this.props.description && <p>Conditions: {this.props.description}</p>}
+//       {this.props.error && <p>Error: {this.props.error}</p>}
+//       </div>
+//     );
+//   }
+// };
+// since this is a stateless component we can simplify the code by using an arrow function.
+// Because we are using an arrow function 'this' no longer works so we get rid of that. the keyword props must also be defined. We pass it in as an argument. Because we are only returning one element 'div', we can get rid of the curly braces and the return keyword and wrap the div in parenthesis.
+const Weather = props => (
+    <div className="weather__info">
+    {
+      props.city && props.country && <p className="weather__key">Location:
+        <span className="weather__value"> {props.city}, {props.country}</span>
+      </p>
+    }
+    {
+      props.temperature && <p className="weather__key">Temperature:
+        <span className="weather__value"> {props.temperature}</span>
+      </p>
+    }
+    {
+      props.humidity && <p className="weather__key">Humidity:
+          <span className="weather__value"> {props.humidity}</span>
+        </p>
+    }
+    {
+      props.description && <p className="weather__key">Conditions:
+        <span className="weather__value"> {props.description}</span>
+      </p>
+    }
+    {
+      props.error && <p className="weather__error">Error: {props.error}</p>
+    }
+    </div>
+  );
 
 export default Weather;
