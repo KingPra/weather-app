d11f5301ecebdcb1c41a2e294a8399f666a5b304
King Pra <kingpra@Kings-MBP.carolina.rr.com>
2018-06-12 13:08:22 -0400

    weather api coming in

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = 'a53130cd66281fe281431da75fa09d0e';
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humdity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    console.log('getWeather running');
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humdity,
+      description: data.weather[0].description,
+      error: ""
+
+
+    })
+  }
+  render () {
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humdity={this.state.humdity}
+          description={this.state.description}
+          error={this.state.error}
+          />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Weather extends React.Component {
+  render () {
+    return (
+      <div>
+      {this.props.city}
+      {this.props.country}
+      {this.props.temperature}
+      {this.props.humidity}
+      {this.props.description}
+      {this.props.error}
+      </div>
+    );
+  }
+};
+
+export default Weather;
