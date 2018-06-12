5a5881f40b6d8c4301f8d583147e9e6f4be1a063
King Pra <kingpra@Kings-MBP.carolina.rr.com>
2018-06-12 16:22:04 -0400

    added mobile styling

--- src/App.js
+++ src/App.js
@@ -48,12 +48,12 @@ class App extends React.Component {
       <div>
         <div className="wrapper">
           <div className="main">
-            <div className="container">
+            <div className="container-fluid">
               <div className="row">
-                <div className="col-xs-5 title-container">
+                <div className="col-xs-12 col-sm-5 title-container">
                   <Titles />
                 </div>
-                <div className="col-xs-7 form-container">
+                <div className="col-xs-12 col-sm-7 form-container">
                   <Form getWeather={this.getWeather}/>
                   <Weather
                     temperature={this.state.temperature}
